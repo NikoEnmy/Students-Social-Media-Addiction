7a29808ffadef027de8ad1bed5548dfa5ceedfdb
niko144 <nikolasboxer99@gmail.com>
2025-06-29 13:35:35 -0600

    Added line of regration for addiction from Mental Health

--- from pandas as pd.py
+++ from pandas as pd.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
 file_path = 'C://Users//nikol//OneDrive//Документы//Students Social Media Addiction.csv'
 data = pd.read_csv(file_path)
-correlation = data['Addicted_Score'].corr(data["Mental_Health_Score"])
-print(f"Corelation between addiction and mental health: {correlation}")
+x = data['Mental_Health_Score']
+y = data['Addicted_Score']
+slope, intercept, r_value, p_value, std_err = linregress(x, y)
 
-plt.scatter(data["Mental_Health_Score"], data['Addicted_Score'], alpha=0.5, c="green")
+plt.scatter(x, y, alpha = 0.5, c='green')
+plt.plot(x, slope * x + intercept, color='red', label=f'y={slope:.2f}x +{intercept:.2f}, r={r_value:.2f}')
 plt.title('Addiction of level of addiction on mental health')
-plt.xlabel('Mental Health Score')
-plt.ylabel('Addicted Score')
+plt.xlabel("Mental Health Score")
+plt.ylabel("Addicted Score")
 plt.grid(True)
+plt.legend()
 plt.show()
+
+print(f"Coefficient correlation: {r_value}")
