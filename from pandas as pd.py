af4b3ebfad8b0bf99007fd994fed19b4f3e4cfd4
niko144 <nikolasboxer99@gmail.com>
2025-06-29 13:10:19 -0600

    added analys of correlation and the chart

--- from pandas as pd.py
+++ from pandas as pd.py
@@ -2,14 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 file_path = 'C://Users//nikol//OneDrive//Документы//Students Social Media Addiction.csv'
 data = pd.read_csv(file_path)
-avg_by_gender = data.groupby('Gender')['Addicted_Score'].mean()
-print("Average level of addiction by gender:")
-print(avg_by_gender)
+correlation = data['Addicted_Score'].corr(data["Mental_Health_Score"])
+print(f"Corelation between addiction and mental health: {correlation}")
 
-avg_by_age = data.groupby('Age')['Addicted_Score'].mean()
-avg_by_age.plot(kind='line', marker='o')
-plt.title('level of addiction by age')
-plt.xlabel('Age')
+plt.scatter(data["Mental_Health_Score"], data['Addicted_Score'], alpha=0.5, c="green")
+plt.title('Addiction of level of addiction on mental health')
+plt.xlabel('Mental Health Score')
 plt.ylabel('Addicted Score')
 plt.grid(True)
 plt.show()
