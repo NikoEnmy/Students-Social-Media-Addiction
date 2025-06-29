7e83931a6b0adc21959f8a878c25e93a876d40a5
niko144 <nikolasboxer99@gmail.com>
2025-06-29 14:06:18 -0600

    added analys by age groups

--- from pandas as pd.py
+++ from pandas as pd.py
@@ -3,16 +3,10 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 file_path = 'C://Users//nikol//OneDrive//Документы//Students Social Media Addiction.csv'
 data = pd.read_csv(file_path)
-avg_by_academic = data.groupby('Academic_Level')['Addicted_Score'].mean()
-avg_by_academic.plot(kind="bar", color="skyblue")
-plt.title("Average level of addiction by academic level")
-plt.xlabel("Academic Level")
-plt.ylabel("Addicted Score")
-plt.show()
-
-avg_by_relationship = data.groupby("Relationship_Status")["Addicted_Score"].mean()
-avg_by_relationship.plot(kind='bar', color="lightgreen")
-plt.title("Average level of addiction by relationship status")
-plt.xlabel('Relationship Status')
+data['Age_Group'] = pd.cut(data['Age'], bins=[17, 20, 24], labels=['18,20', "21-24"])
+avg_by_age_group = data.groupby('Age_Group')["Addicted_Score"].mean()
+avg_by_age_group.plot(kind="bar", color="orange")
+plt.title("Average level of addiction by age group")
+plt.xlabel("Age group")
 plt.ylabel("Addicted Score")
 plt.show()
